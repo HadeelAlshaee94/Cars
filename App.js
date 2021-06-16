806b8cc2d5855595cf3818f3f200d6dda5d928a6
HadeelAlshaee94 <>
2021-06-16 12:40:11 +0000

    changess

---
+++ App.js
@@ -0,0 +1,29 @@
+
+
+var name = prompt("What is name of car you want ?")
+alert("welcome to Cars website " + name)
+
+var Carwant = prompt("what is your fav car from these (SEDAN, Sport Car , Coupe)?")
+
+
+if(Carwant == "SEDAN"){
+  document.write("<p>"+ "great thing you love SEDAN car"+"</p>")
+}
+ else if(Carwant == "Sport Car")
+{
+ document.write("<div>"+"<h3>"+"Sport Car"+"</h3>"+
+        "<img src='https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/body-image/public/1-corvette-stingray-c8-2019-fd-hr-hero-front_0.jpg?itok=SEYe_vLy' >"+
+      "</div>"
+ )
+
+
+}
+
+else if(Carwant == "Coupe")
+{
+ document.write("<p>"+ "great thing you love Coupe car"+"</p>")
+}
+
+else{
+  alert("sorry we don't have this car")
+}
